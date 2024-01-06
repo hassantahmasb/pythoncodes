1e392e2fe88f5bc810ffc6fa53729ca05784c68e
Hassan Tahmasb <32610112+hassantahmasb@users.noreply.github.com>
2024-01-06 12:27:18 +0330

    Update main.py
    Add Hadi

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     habits: list[Habit] = [
         habit_tracker("Smoke", datetime(2023,10,15,8), 12000, 30),
         habit_tracker("instagram", datetime(2023,11,20,6), 20000, 120),
+        habit_tracker("Hadi", datetime(2023,11,20,6), 20000, 120),
     ]
     df = pd.DataFrame(habits)
     print(tabulate(df,headers="keys",tablefmt="heavy_grid"))
