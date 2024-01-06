cee72f9bc418fea014eeb72cbed189a80e499881
hassantahmasb <hsn.tsb@gmail.com>
2024-01-06 12:32:18 +0330

    added result to readme note file

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from datetime import datetime
 
 def main():
     habits: list[Habit] = [
-        habit_tracker("Smoke", datetime(2023,10,15,8), 12000, 30),
-        habit_tracker("instagram", datetime(2023,11,20,6), 20000, 120),
-        habit_tracker("Hadi", datetime(2023,11,20,6), 20000, 120),
+        habit_tracker("Smoke", datetime(2023, 10, 15, 8), 12000, 30),
+        habit_tracker("instagram", datetime(2023, 11, 20, 6), 20000, 120),
+        habit_tracker("Hadi", datetime(2023, 11, 20, 6), 20000, 120),
     ]
     df = pd.DataFrame(habits)
-    print(tabulate(df,headers="keys",tablefmt="heavy_grid"))
+    print(tabulate(df, headers="keys", tablefmt="heavy_grid"))
+
 
 if __name__ == '__main__':
     main()
